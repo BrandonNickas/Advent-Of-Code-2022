6d9e2b86787be5b822864c21b69b6833f06a999f
Brandon Nickas <brandonnickas@gmail.com>
2022-12-04 01:10:51 -0500

    Day 4

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -17,9 +17,9 @@ def part2():
 
     completeOverlap = 0
     for line in lines:
-        range1 = line.split(',')[0].split('-')
-        range2 = line.split(',')[1].split('-')
-        if int(range1[0]) <= int(range2[0]) <= int(range1[1]) or int(range1[0]) <= int(range2[1]) <= int(range1[1]) or int(range2[0]) <= int(range1[0]) <= int (range2[1]) or int(range2[0]) <= int(range1[1]) <= int(range2[1]):
+        range1 = [int(number) for number in line.split(',')[0].split('-')]
+        range2 = [int(number) for number in line.split(',')[1].split('-')]
+        if range1[0]<= range2[0] <= range1[1] or range1[0] <= range2[1] <= range1[1] or range2[0] <= range1[0] <= range2[1] or range2[0] <= range1[1] <= range2[1]:
             completeOverlap += 1
 
     print(completeOverlap)
