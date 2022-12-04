6585a50a06a50efc72b731715fe28e6b41fd9923
Brandon Nickas <brandonnickas@gmail.com>
2022-12-04 00:59:15 -0500

    Day 3

---
+++ Day4/Day4.py
@@ -0,0 +1,28 @@
+def part1():
+    with open('input.txt') as f:
+        lines = f.read().splitlines()
+
+    completeOverlap = 0
+    for line in lines:
+        range1 = line.split(',')[0].split('-')
+        range2 = line.split(',')[1].split('-')
+        if (int(range1[0]) >= int(range2[0]) and int(range1[1]) <= int(range2[1])) or (int(range2[0]) >= int(range1[0]) and int(range2[1]) <= int(range1[1])):
+            completeOverlap += 1
+
+    print(completeOverlap)
+
+def part2():
+    with open('input.txt') as f:
+        lines = f.read().splitlines()
+
+    completeOverlap = 0
+    for line in lines:
+        range1 = line.split(',')[0].split('-')
+        range2 = line.split(',')[1].split('-')
+        if int(range1[0]) <= int(range2[0]) <= int(range1[1]) or int(range1[0]) <= int(range2[1]) <= int(range1[1]) or int(range2[0]) <= int(range1[0]) <= int (range2[1]) or int(range2[0]) <= int(range1[1]) <= int(range2[1]):
+            completeOverlap += 1
+
+    print(completeOverlap)
+
+part1()
+part2()
